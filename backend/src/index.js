63eab4c7cb98c4e11644a4c5f9bbafc72c665015
wahb-amir <op422010@gmail.com>
2026-05-29 17:39:34 +0500

    fix: moved backend/server.js to backend/src/index.js to match ci scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const dotenv = require("dotenv");
 // const dbConnection = require("./utils/dbConnection");
-const dbConnection = require("./src/utils/dbConnection")
+const dbConnection = require("./utils/dbConnection")
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
