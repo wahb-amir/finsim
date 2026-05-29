0d7727dabf455ccfae259541ece3e715f64cc5d5
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-05-29 16:16:20 +0500

    making server.js and dbConnection

---
+++ backend/utils/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const dotenv = require("dotenv");
+const dbConnection = require("./dbConnection");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT;
+
+//Middleware
+dotenv.config();
+app.use(cors())
+app.use(cookieParser())
+
+//RateLimit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later.",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
+
+//Mongoose
+dbConnection();
+
+//Server Listen
+app.listen(PORT, (err)=>{
+    if(err){
+        console.err("❌❌ Server Can't Connnected");
+    } else{
+        console.log("✅✅ Server Connected")
+    }
+})
